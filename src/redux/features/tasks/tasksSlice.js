9f1d1d854dde22dd1b06da190919b86745f024e6
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2025-02-18 08:12:15 +0600

    add: task add to redux state functionality

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -7,7 +7,22 @@ const initialState = {
 const tasksSlice = createSlice({
   name: "tasks",
   initialState,
-  reducers: {},
+  reducers: {
+    addTask: (state, { payload }) => {
+      if (state.tasks.length === 0) {
+        state.tasks.push({ id: 1, status: "pending", ...payload });
+      } else {
+        const lastElement = state.tasks.at("-1");
+        state.tasks.push({
+          id: lastElement.id + 1,
+          status: "pending",
+          ...payload,
+        });
+      }
+    },
+  },
 });
+
+export const { addTask } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
