43d6a349ba87e85dd911d27c3dde3d9d81004407
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2025-03-12 22:38:20 +0600

    add: RTQ query setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./features/tasks/tasksSlice";
 import userReducer from "./features/users/userSlice";
+import { baseApi } from "./features/api/baseApi";
 const store = configureStore({
   reducer: {
     taskReducer,
     userReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export default store;
